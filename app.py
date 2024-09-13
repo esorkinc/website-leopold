b04367753a4a9c68b712eceda611f2aa75dac804
esorkinc <esorkinc@gmail.com>
2024-09-13 00:42:50 -0500

    Updated app.py

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
-import os
 import streamlit as st
-
-# Get the port from the environment variable
-port = int(os.environ.get("PORT", 8501))
-
-# Run Streamlit with the assigned port
-st.run(port=port, host="0.0.0.0")
-
 
 st.title("My First Web App")
 st.write("Hello, friend")
